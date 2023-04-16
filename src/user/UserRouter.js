e8332f0a9b18e1375b4879ecd37c68ec571c55e3
rafaa1999 <raferhouma10@gmail.com>
2023-04-16 02:20:57 +0100

    validation

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -5,8 +5,29 @@ const router = express.Router();
 const pagination = require('../shared/pagination');
 const idNumberControl = require('../shared/idNumberControl');
 const UserService = require('./UserService');
+const { body, validationResult } = require('express-validator');
+const ValidationException = require('../shared/ValidationException');
 
-router.post('/users', async (req, res) => {
+router.post('/users', 
+body('username')
+  .notEmpty().withMessage('Username cannot be null')
+  .bail()
+  .isLength({min: 4, max: 32}).withMessage('Username must have min 4 max 32 characters'),
+body('email')
+  .isEmail().withMessage('Must be a valid e-mail address')
+  .bail()
+  .custom(async (email) => {
+    const user = await UserService.findByEmail(email)
+    if(user){
+      throw new Error('Email in use');
+    }
+  })
+  ,
+async (req, res, next) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return next(new ValidationException(errors.array()));
+  }
   await UserService.create(req.body);
   res.send("success");
 })
